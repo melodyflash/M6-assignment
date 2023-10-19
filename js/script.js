e61e8f34e499b6c2190ea89ce8462b0656814a4c
Melody G <melodyflash@gmail.com>
2023-10-19 05:48:55 -0700

    comments added

--- js/script.js
+++ js/script.js
@@ -9,8 +9,9 @@ ext              = $('ext')
 email            = $('email')
 department       = $('department')
 empForm          = $('empForm')
-// // display info entered
+// display info entered on console
 const displayInfo = () => (console.log(`ID: ${idNum.value} \nName: ${fullName.value}\nExt: ${ext.value}\nEmail: ${email.value}\nDepartment: ${department.value}`))
+// event listener attached to form on submit
 empForm.addEventListener('submit', (e) => {
     e.preventDefault()
     displayInfo()
