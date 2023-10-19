3e3eca85296d4fee719c0b56c6a6bfadb462a30b
Melody G <melodyflash@gmail.com>
2023-10-19 05:45:50 -0700

    modified and done

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
 // GLOBAL VARIABLES
 let idNum, fullName, ext, email, department, empForm
-
 // HELPER FUNCTION TO RETURN DOM ELEMENT
 const $ = (id) => document.getElementById(id)
-
 // GET THE DOM ELEMENTS
 idNum            = $('idNum')
 fullName         = $('fullName')
 ext              = $('ext')
 email            = $('email')
 department       = $('department')
-// submit           = $('submit')
 empForm          = $('empForm')
-
 // // display info entered
 const displayInfo = () => (console.log(`ID: ${idNum.value} \nName: ${fullName.value}\nExt: ${ext.value}\nEmail: ${email.value}\nDepartment: ${department.value}`))
-
 empForm.addEventListener('submit', (e) => {
     e.preventDefault()
     displayInfo()
